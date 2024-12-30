336c8aa9fa60d7be076695af88435e7e96da6d6c
Youngjae <2015fromjeki@gmail.com>
2024-12-30 17:45:01 +1100

    Set up the Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+// Pages
+import ProfilePage from "./pages/Profile.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 const App = () => {
   return (
     <div>
-      <p className="bg-red-400">Hello</p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
       <ToastContainer />
     </div>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+import { MessageCircle } from "lucide-react";
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl text-blue-300">
+          <p className="text-2xl font-bold tracking-wide">Chatty</p>
+          <MessageCircle className="fill-red-200 text-red-500" />
+        </a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1"></ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
